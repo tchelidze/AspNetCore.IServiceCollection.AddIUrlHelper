f12429647edc72de21932549a01a0d264a83481a
bitchiko tchelidze <bubachelidze1@gmail.com>
2017-10-18 11:10:45 +0400

    Add project files.

---
+++ AspNetCore.IServiceCollection.AddIUrlHelper/IServiceCollectionUrlHelperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCore.IServiceCollection.AddIUrlHelper
+{
+    public static class IServiceCollectionUrlHelperExtensions
+    {
+        public static Microsoft.Extensions.DependencyInjection.IServiceCollection AddUrlHelper(this Microsoft.Extensions.DependencyInjection.IServiceCollection services)
+        {
+            services
+                .AddSingleton<IActionContextAccessor, ActionContextAccessor>()
+                .AddScoped(it =>
+                    it
+                        .GetRequiredService<IUrlHelperFactory>()
+                        .GetUrlHelper(it.GetRequiredService<IActionContextAccessor>().ActionContext));
+            return services;
+        }
+    }
+}
